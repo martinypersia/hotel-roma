580793df20e358f67c51886e83e699137d80b2c2
Tici Angelucci <tittiangelucci08@gmail.com>
2023-05-30 19:02:52 -0300

    gestion vista 1/2

---
+++ gestion.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/fontello.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="container">
+            <h2>WEEEEN dia</h2>
+        </div>
+    </header>
+    <div class="capa"></div>
+
+
+</body>
+
+</html>
