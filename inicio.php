3a2b349994949076562cdbd46a11793ed7e2ce9e
ticiAngelucci <tittiangelucci08@gmail.com>
2023-06-03 16:37:35 -0300

    fix navbar no responsive

--- inicio.php
+++ inicio.php
@@ -4,10 +4,30 @@
         <img src="assets/logo.png" alt="logoHotel" width="228px">
     </header>
     <ul>
-        <li><a href="#"><i class="fa-solid fa-gauge-high"></i>Dashboard</a></li>
-        <li><a href="#"><i class="fa-solid fa-bell-concierge"></i>Gestión</a></li>
-        <li><a href="#"><i class="fa-solid fa-wrench"></i>Mantenimiento</a></i></li>
-        <li><a href="#"><i class="fa-solid fa-user"></i>Usuarios</a></i></li>
+        <li><a class="nav-link" href="#" style="color: white;"><i class="fa-solid fa-gauge-high"></i>Dashboard</a></li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false" style="color: white;">
+                <i class="fa-solid fa-bell-concierge"></i>
+                Gestión
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Recepción</a>
+                <a class="dropdown-item" href="#">Salida</a>
+            </div>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false" style="color: white;">
+                <i class="fa-solid fa-wrench"></i>Mantenimiento
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Habitaciones</a>
+                <a class="dropdown-item" href="#">Categoria</a>
+                <a class="dropdown-item" href="#">Pisos</a>
+            </div>
+        </li>
+        <li><a class="nav-link" href="#" style="color: white;"><i class="fa-solid fa-user"></i>Usuarios</a></i></li>
     </ul>
 </div>
 
